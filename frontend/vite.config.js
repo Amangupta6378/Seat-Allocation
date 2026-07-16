4184a9a2e6b6c18f50d3a91f74b4ae2503aaac37
Amangupta6378 <guptaman6378@gmail.com>
2026-07-16 14:51:30 +0530

    warning

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,26 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 800,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('recharts')) {
+              return 'recharts';
+            }
+
+            if (id.includes('react-router-dom')) {
+              return 'router';
+            }
+
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
   server: {
     port: 3000,
     proxy: {
